d05425d56c2550f81afe786a5a90c19f458eaa24
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-14 02:56:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,13 @@
-require 'pry'
-
 def consolidate_cart(cart)
-
+grocery_cart = {}
+cart.each{|grocery_item| grocery_item.each 
+{|item, item_hash| grocery_cart[item] => item_hash
+if grocery_cart[item][:count]
+  grocery_cart[item][:count] +=1
+else
+  grocery_cart[item][:count]=1
+}}
+grocery_cart
 end
 
 def apply_coupons(cart, coupons)
