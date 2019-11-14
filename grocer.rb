d87ca7b3c0bb2d1ba6246d9c0fb09fac74d47139
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-14 04:57:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,8 +21,8 @@ def apply_coupons(cart, coupons)
       cart[item][:count] -= coupon[:num]
     elsif cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"][:count] += coupon[:num]
-      cart[item][:count] -= coupon[:num]
     end
+     cart[item][:count] -= coupon[:num]
   end 
   cart
 end
