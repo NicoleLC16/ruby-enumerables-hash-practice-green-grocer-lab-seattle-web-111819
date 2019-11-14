8f0936c5ad82717be7ce13b8439a05fad40519ad
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-14 05:37:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,5 +44,7 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(new_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
   
-  total = applied_discount.reduce(0) {|acc, (key, value)| }
+  total = applied_discount.reduce(0) {|acc, (key, value)| acc += value[:price] * value[:count]}
+  
+  total > 100 ? total * 0.9 : total
 end
