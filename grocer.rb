7ad08d7f71e2233fa8894f2a5ac97d91142751cb
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-14 05:24:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,7 +34,7 @@ end
 
 def apply_clearance(cart)
   cart.each do |item, item_hash|
-    item_hash[:price] -= stats[:price] * 0.2 if stats[:clearance] 
+    item_hash[:price] -= stats[:price] * 0.2 if item_hash[:clearance] 
     end
   cart
 end
